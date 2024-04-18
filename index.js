f6cfd4275d5cdf36347d96508fe73fc08d9161f9
Zamzam Abdiwahid <zamzamabdiwahid123@gmail.com>
2024-04-18 14:06:50 +0300

    Create DOM elements programmatically,Add elements to the DOM

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+let mainElement = document.getElementById('main');
+mainElement.remove();
+
+let newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'YOUR-NAME is the champion'; 
+document.body.appendChild(newHeader);
